4b6cf40fe53e7704b7c14bd716c073b6d391b1c4
Ekaterina <ekaterina.kuz@gmail.com>
2024-12-31 17:57:09 +0300

    Small amendments

--- src/processing.py
+++ src/processing.py
@@ -15,7 +15,5 @@ def filter_by_state(list_dict: List[Dict], key: str = "EXECUTED") -> List[Dict]:
 def sort_by_date(list_dict: List[Dict], sort_arg: bool = True) -> List[Dict]:
     """Принимает список словарей и параметр сортировки(по умолчанию "True" — 'CANCELED').
     Функция возвращает новый список, отсортированный по дате"""
-    sort_list = sorted(
-        list_dict, key=lambda each_dict: each_dict["date"], reverse=sort_arg
-    )
+    sort_list = sorted(list_dict, key=lambda each_dict: each_dict["date"], reverse=sort_arg)
     return sort_list
